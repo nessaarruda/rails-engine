46b445ba9d4fe859e4b031dc0644955ba547c026
Vanessa Arruda <wanessaarruda@gmail.com>
2021-02-12 14:32:30 -0700

    update expectation for 20 per page test

--- spec/features/api/v1/merchants/all_merchants_spec.rb
+++ spec/features/api/v1/merchants/all_merchants_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'Get all merchants', type: :request do
   describe 'happy path' do
-    it 'can return all merchants' do
+    it 'can return all merchants 20 per page' do
       create_list(:merchant, 100)
 
-      get '/api/v1/merchants/merchants'
+      get '/api/v1/merchants/merchants', params: { limit: 20 }
+
+      merchants = JSON.parse(response.body)
 
       expect(response).to have_http_status(:success)
-      expect(Merchant.all.count).to eq(20)
+      expect(merchants['data'].count).to eq(20)
     end
   end
 end
