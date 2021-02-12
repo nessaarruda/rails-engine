49be2e2101a0dda16b86bf2c8b14cfe00c21d3b0
Vanessa Arruda <wanessaarruda@gmail.com>
2021-02-12 14:12:36 -0700

    Revisit find all merchants test to test if it is showing 20 at a time

--- spec/features/api/v1/merchants/all_merchants_spec.rb
+++ spec/features/api/v1/merchants/all_merchants_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe 'Get all merchants', type: :request do
   describe 'happy path' do
     it 'can return all merchants' do
-      create_list(:merchant, 10)
+      create_list(:merchant, 100)
 
       get '/api/v1/merchants/merchants'
 
       expect(response).to have_http_status(:success)
-      expect(Merchant.all.count).to eq(10)
+      expect(Merchant.all.count).to eq(20)
     end
   end
 end
