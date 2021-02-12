714a6d013234dabb9c9cc816b1beb1a15773b4c8
Vanessa Arruda <wanessaarruda@gmail.com>
2021-02-12 13:17:53 -0700

    update routes to use find controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
 
       namespace :merchants do
-        get '/merchants', to: 'api/v1/merchants/merchants_controller#index'
+        get '/find_all', to: 'find#index'
       end
 
     end
