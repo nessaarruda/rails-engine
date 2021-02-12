fbf71fe20ef34bcbd140fe0d9ead71da7a9cec81
Vanessa Arruda <wanessaarruda@gmail.com>
2021-02-12 16:09:47 -0700

    Update test to check for merchant details

--- spec/features/api/v1/items/merchant_details_for_one_item_spec.rb
+++ spec/features/api/v1/items/merchant_details_for_one_item_spec.rb
@@ -1,25 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe 'Get all items for one merchants', type: :request do
+RSpec.describe 'Get merchant details for one item', type: :request do
   describe 'happy path' do
-    it 'can return items for one merchant based on id' do
-      merchant1 = create(:merchant)
-      items1 = create_list(:item, 5, merchant_id: merchant1.id)
-      merchant2 = create(:merchant)
-      items2 = create_list(:item, 10, merchant_id: merchant2.id)
+    it 'can return merchant details for one item' do
+      merchant = create(:merchant)
+      item = create(:item, merchant_id: merchant.id)
 
-      get "/api/v1/merchants/#{merchant1.id}/items"
+      get "/api/v1/items/#{item.id}/merchants"
 
       expect(response).to have_http_status(:success)
 
-      items = JSON.parse(response.body)
+      merchant_details = JSON.parse(response.body)
 
-      expect(items['data'][0]['attributes']['merchant_id']).to eq(merchant1.id)
-      expect(items['data'][1]['attributes']['merchant_id']).to eq(merchant1.id)
-      expect(items['data'][2]['attributes']['merchant_id']).to eq(merchant1.id)
-      expect(items['data'][3]['attributes']['merchant_id']).to eq(merchant1.id)
-      expect(items['data'][4]['attributes']['merchant_id']).to eq(merchant1.id)
-      expect(items2.first.id).to_not eq(merchant1.id)
+      expect(response).to have_http_status(:success)
+      expect(merchant_details['data']['id'].to_i).to eq(merchant.id)
+      expect(merchant_details['data']['type']).to eq('merchant')
+      expect(merchant_details['data']['attributes']).to be_a(Hash)
+      expect(merchant_details['data']['attributes']['name']).to eq(merchant.name)
     end
   end
 end
