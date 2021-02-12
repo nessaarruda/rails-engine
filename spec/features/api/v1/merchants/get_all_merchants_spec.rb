2860baf9d94f3f8cd6d779048413b8e14a32c0ed
Vanessa Arruda <wanessaarruda@gmail.com>
2021-02-12 12:33:02 -0700

    Update get all merchant tests to use faker

--- spec/features/api/v1/merchants/get_all_merchants_spec.rb
+++ spec/features/api/v1/merchants/get_all_merchants_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe 'Get all merchants' do
+RSpec.describe 'Get all merchants', type: :request do
   describe 'happy path' do
+    let!(:merchants) { FactoryBot.create_list(:random_merchant, 100)}
+    before {get '/api/v1/merchants'}
     it 'can return all merchants' do
-        merchant1 = create(:merchant, name: 'All Things')
-        merchant2 = create(:merchant, name: 'Few Things')
-
-    get '/api/v1/merchants'
-
-    expect(response.status).to eq(200)
+      expect(JSON.parse(response.body).size).to eq(100)
+    end
+    it 'returns status code 200' do
+      expect(response).to have_http_status(:success)
     end
   end
 end
