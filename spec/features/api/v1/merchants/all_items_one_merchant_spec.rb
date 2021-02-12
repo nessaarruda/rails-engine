e3f96e00c479203ac38d6c0822b5ebade7789715
Vanessa Arruda <wanessaarruda@gmail.com>
2021-02-12 15:39:28 -0700

    spec

--- spec/features/api/v1/merchants/all_items_one_merchant_spec.rb
+++ spec/features/api/v1/merchants/all_items_one_merchant_spec.rb
@@ -3,13 +3,23 @@ require 'rails_helper'
 RSpec.describe 'Get all items for one merchants', type: :request do
   describe 'happy path' do
     it 'can return items for one merchant based on id' do
-      merchant = create(:merchant)
+      merchant1 = create(:merchant)
+      items1 = create_list(:item, 5, merchant_id: merchant1.id)
+      merchant2 = create(:merchant)
+      items2 = create_list(:item, 10, merchant_id: merchant2.id)
 
-      get "/api/v1/merchants/#{merchant.id}/items"
+      get "/api/v1/merchants/#{merchant1.id}/items"
 
       expect(response).to have_http_status(:success)
 
       items = JSON.parse(response.body)
+
+      expect(items['data'][0]['attributes']['merchant_id']).to eq(merchant1.id)
+      expect(items['data'][1]['attributes']['merchant_id']).to eq(merchant1.id)
+      expect(items['data'][2]['attributes']['merchant_id']).to eq(merchant1.id)
+      expect(items['data'][3]['attributes']['merchant_id']).to eq(merchant1.id)
+      expect(items['data'][4]['attributes']['merchant_id']).to eq(merchant1.id)
+      expect(items2.first.id).to_not eq(merchant1.id)
     end
   end
 end
