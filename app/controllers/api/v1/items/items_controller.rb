4bf07547faa8c2a2b07a96a058800919cf78c551
Vanessa Arruda <wanessaarruda@gmail.com>
2021-02-12 20:54:34 -0700

    update method

--- app/controllers/api/v1/items/items_controller.rb
+++ app/controllers/api/v1/items/items_controller.rb
@@ -8,4 +8,22 @@ class Api::V1::Items::ItemsController < ApplicationController
   def show
     render json: ItemSerializer.new(Item.find(params[:id]))
   end
+
+  def create
+    render json: ItemSerializer.new(Item.create(item_params))
+  end
+
+  def update
+    render json: ItemSerializer.new(Item.update(params[:id], item_params))
+  end
+
+  def destroy
+    Item.destroy(params[:id])
+  end
+
+  private
+
+  def item_params
+    params.permit(:id, :name, :description, :unit_price, :merchant_id)
+  end
 end
