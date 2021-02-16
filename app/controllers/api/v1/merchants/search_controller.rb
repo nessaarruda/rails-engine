246cb46e3bf93387476710632e051c06157eb1ab
Vanessa Arruda <wanessaarruda@gmail.com>
2021-02-16 14:58:30 -0700

    add helpers to locate attribute and value

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -3,4 +3,18 @@ class Api::V1::Merchants::SearchController < ApplicationController
   def find_merchant
     render json: MerchantSerializer.new(Merchant.search_one(attribute, value))
   end
+
+  private
+
+  def find_params
+    params.permit(:name)
+  end
+
+  def attribute
+    find_params.keys[0]
+  end
+
+  def value
+    find_params[attribute]
+  end
 end
