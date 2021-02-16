a431538d79a862513c66ba281af5c370c760e3b4
Vanessa Arruda <wanessaarruda@gmail.com>
2021-02-16 13:57:14 -0700

    dream drive search one method

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Merchants::SearchController < ApplicationController
 
   def find_merchant
-
+    render json: MerchantSerializer.new(Merchant.search_one(attribute, value))
   end
 end
