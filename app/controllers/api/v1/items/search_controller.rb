df798755c75970a3c884fe71a2cfbf91c3b7b102
Vanessa Arruda <wanessaarruda@gmail.com>
2021-02-18 20:24:26 -0700

    line

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -2,6 +2,5 @@ class Api::V1::Items::SearchController < ApplicationController
   def find_items
     item = Item.where('LOWER(name) LIKE ?', "%#{params[:name].downcase}%")
     render json: ItemSerializer.new(item)
-    #return 0 if nothing found
   end
 end
